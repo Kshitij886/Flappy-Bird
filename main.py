6f7f824ddcadda525bfc52d7e9a25e21bc0e72e3
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-20 12:06:48 +0545

    feat: put score in window

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import neat
 import time
 import random
 import os
+pygame.font.init()
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
@@ -11,6 +12,9 @@ BIRD_IMGS = [ pygame.transform.scale2x(pygame.image.load(os.path.join("assets", 
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bg.png")))
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
+
 
 class Bird:
     IMGS = BIRD_IMGS
@@ -149,11 +153,14 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, bird, pipes, base):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0,0))
     
     for pipe in pipes:
         pipe.draw(win)
+    
+    text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
+    win.blit(text,(WIN_WIDTH - 10 - text.get_width(), 10))
     
     base.draw(win)
 
@@ -199,8 +206,8 @@ def main():
             pipes.remove(r)
 
         if bird.y + bird.img.get_height() >= 730:
-            pass
-        draw_window(win,bird,pipes, base)
+            pass 
+        draw_window(win,bird,pipes, base, score)
 
     pygame.quit()
     quit()
