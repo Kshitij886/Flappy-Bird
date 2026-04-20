4f9c6a9e4e93c97df573868309f52ecbc559a222
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-20 10:38:45 +0545

    feat: collide with pipe logic

--- main.py
+++ main.py
@@ -86,7 +86,6 @@ class Pipe:
     def __init__(self, x):
         self.x = x
         self.height = 0
-        self.gap = 100
 
         self.top = 0
         self.bottom = 0
@@ -107,6 +106,21 @@ class Pipe:
     def draw(self, win):
         win.blit(self.PIPE_TOP, (self.x, self.top))
         win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset) 
+        t_point = bird_mask.overlap(top_mask, top_offset) 
+
+        if t_point or b_point:
+            return True
+        return False
 
 
 
