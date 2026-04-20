6c51bd3e7387bfbec1b0d9ef1fed8159f86db5ba
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-20 09:48:28 +0545

    feat: made python object for bird

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame;
+import neat
+import time
+import random
+import os
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMGS = [ pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bird3.png"))) ]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("assets", "bg.png")))
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5 
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count
+        self.img = self.IMGS[0]
+
+    def jump(self): 
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
